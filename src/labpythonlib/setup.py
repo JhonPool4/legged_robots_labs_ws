5933e1880fc7f984b572fe5007c61c9ff3a9933f
JhonPool4 <jhoncharaja@gmail.com>
2021-10-16 12:07:35 -0500

    new version 0.0.2

--- src/labpythonlib/setup.py
+++ src/labpythonlib/setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='labpythonlib',
-    version='0.0.1',
-    description='Library with useful functions for laboratory',
+    version='0.0.2',
+    description='Library with useful functions for laboratories',
     url='',
     author='Jhon Charaja',
     author_email='jhon.charaja@usp.br',
